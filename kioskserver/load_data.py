7d530255ab30cccac463206b2f78a9099f22f4a7
Liam <22pilarskil@millburn.org>
2021-05-04 10:09:36 -0400

    Add new data loader

--- kioskserver/load_data.py
+++ kioskserver/load_data.py
@@ -1,6 +1,7 @@
 import csv
 import api.models
 
+FILENAME = "/home/kiosk/kioskv3/roster.csv"
 with open(FILENAME) as csv_file:
 	reader = csv.reader(csv_file)
 	for row in reader:
@@ -9,7 +10,14 @@ with open(FILENAME) as csv_file:
 		grade = row[2]
 		privilege_granted = True if int(grade)==12 else False
 		pathToImage = ""
-		models.Student.objects.create(name=name, grade=grade, student_id=student_id,privilege_granted=privilege_granted,pathToImage="")
+		if (len(Student.objects.all().filter(pk=student_id)) == 0):
+			models.Student.objects.create(name=name, grade=grade, student_id=student_id,privilege_granted=privilege_granted,pathToImage="")
 
-
-		
+		else:
+			s = Student.objects.all().get(pk=student_id)
+			s.name = name
+			s.student_id = student_id
+			s.grade = grade
+			s.privilege_granted = privilege_granted
+			s.pathToImage = pathToImage
+			s.save()
